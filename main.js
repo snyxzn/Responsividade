335a6d8038322847f9d19dfe89aa7bb8325d8165
ricardo <115740144+snyxzn@users.noreply.github.com>
2023-05-15 14:23:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuHamburger = document.querySelector('.hamburger-menu');
+
+const menu = document.querySelector('.menu');
+
+menuHamburger.addEventListener('click', () => {
+
+menu.classList.toggle('ativo')
+
+});
